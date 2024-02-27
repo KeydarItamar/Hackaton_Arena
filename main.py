a90b44f8691a9b94e164d8adeb32978f8e7c3393
marta34 <marta@trkkn.com>
2024-02-27 16:11:13 +0100

    verification_location

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
-from client import getLocNum  # Import the getLocNum function
+from client import getLocNum, is_there
 
 app = FastAPI()
 
 @app.get("/getUbiByNum/{num}")
-async def read_root(num: str):
+def read_root(num: str):
     try:
-        location = await getLocNum(num)
+        location = getLocNum(num)
         return location
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.get("/is_there/{device_num}/{longitude}/{latitude}")
+def verify_location(device_num: str, longitude: float, latitude: float):
+    radius=300
+    try:
+        is_within = is_there(device_num, longitude, latitude,radius)
+        return {"device_present": is_within}
     except Exception as e:
         return {"error": str(e)}
 
